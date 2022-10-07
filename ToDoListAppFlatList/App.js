0f76a1eb81b32272c1f739d121638e629741481b
phuong <phuongdenpro@gmail.com>
2022-10-07 10:52:25 +0700

    first commit

---
+++ ToDoListAppFlatList/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  Alert,
+  FlatList,
+} from "react-native";
+import Task from "./components/Task";
+import styles from "./App.components.style";
+import Form from "./components/Form";
+
+const App = () => {
+  const [taskList, setTaskList] = useState([]);
+  const handleAddTask = (task) => {
+    setTaskList([...taskList, task]);
+  };
+  const handleDeleteTask = (index) => {
+    Alert.alert("Thông báo !", "Bạn có chắc chắn muốn xóa?", [
+      {
+        text: "OK",
+        onPress: () => {
+          let taskListTmp = [...taskList];
+          taskListTmp.splice(index, 1);
+          setTaskList(taskListTmp);
+        },
+      },
+      { text: "Cancel", onPress: () => {} },
+    ]);
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.body}>
+        <Text style={styles.header}>Todo List</Text>
+        <FlatList
+          data={taskList}
+          renderItem={({ item }) => (
+            <Task
+              key={taskList.indexOf(item)}
+              title={item}
+              number={taskList.indexOf(item)+1}
+              onDeleteTask={() => handleDeleteTask(taskList.indexOf(item))}
+            />
+          )}
+        />
+      </View>
+      <Form onAddTask={handleAddTask} />
+
+      <StatusBar style="dark" />
+    </View>
+  );
+};
+
+export default App;
